228c80f01ebd8b1da14e64580cf9def9a34e4470
Adrien75-lab <adrichristophe@gmail.com>
2022-10-28 08:54:36 +0200

    fitness-app react

---
+++ src/components/pages/Home.js
@@ -0,0 +1,27 @@
+import { Box } from "@mui/material";
+import React, { useState } from "react";
+import Exercises from "../Exercises";
+import HeroBanner from "../HeroBanner";
+import SearchExercises from "../SearchExercises";
+
+const Home = () => {
+  const [bodyPart, setBodyPart] = useState("all");
+  const [exercises, setExercises] = useState([]);
+  return (
+    <Box>
+      <HeroBanner />
+      <SearchExercises
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart}
+      />
+      <Exercises
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+        exercises={exercises}
+      />
+    </Box>
+  );
+};
+
+export default Home;
